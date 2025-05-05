baf2a2792bc260c5279bb8fa30ca4661ee894946
RaftDev <theraft08@gmail.com>
2025-05-05 12:44:13 +0200

    Don't include modloaders that have the classpath field on false

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://files.atlasworld.fr/crafted/launcher/legacy/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://cdn.atlasworld.fr/crafted/launcher/legacy/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
